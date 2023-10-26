720150c8d69b307d4f8b5a98f657291b30a337ae
abhinav3254 <abhinav3254@gmail.com>
2023-10-26 18:31:50 +0530

    comments

--- src/main/java/movie/service/impl/MovieServiceImpl.java
+++ src/main/java/movie/service/impl/MovieServiceImpl.java
@@ -13,47 +13,64 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
+/**
+ * The MovieServiceImpl class implements the MovieService interface and provides
+ * methods for managing movie-related operations, including adding movies and
+ * retrieving movies by various criteria.
+ */
 @Service
 public class MovieServiceImpl implements MovieService {
 
     @Autowired
     private MovieRepository movieRepository;
+
+    /**
+     * Add a list of movies to the system.
+     *
+     * @param movies The list of movies to be added.
+     * @return A ResponseEntity with a message indicating the status of the addition.
+     */
     @Override
     public ResponseEntity<String> addMovies(List<Movie> movies) {
         try {
-
             for (int i = 0; i < movies.size(); i++) {
                 movieRepository.save(movies.get(i));
             }
-
-            return new ResponseEntity<>("added "+movies.size()+" movies",HttpStatus.CREATED);
-
+            return new ResponseEntity<>("added " + movies.size() + " movies", HttpStatus.CREATED);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    /**
+     * Add a single movie to the system.
+     *
+     * @param movie The movie to be added.
+     * @return A ResponseEntity with a message indicating the status of the addition.
+     */
     @Override
     public ResponseEntity<String> addMovie(Movie movie) {
         try {
-
             movieRepository.save(movie);
-            return new ResponseEntity<>("added movie",HttpStatus.CREATED);
-
+            return new ResponseEntity<>("added movie", HttpStatus.CREATED);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    /**
+     * Retrieve a paginated list of movies.
+     *
+     * @param pageable Pageable information for pagination.
+     * @return A ResponseEntity containing a Page of movies.
+     */
+    @Override
     public ResponseEntity<Page<Movie>> getMovies(Pageable pageable) {
         try {
-            // Simulate a delay of 2 seconds (optional)
-            // Thread.sleep(2000);
-
             Page<Movie> movies = movieRepository.findAll(pageable);
-
             return new ResponseEntity<>(movies, HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
@@ -61,6 +78,15 @@ public class MovieServiceImpl implements MovieService {
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    /**
+     * Retrieve movies by release year in a paginated manner.
+     *
+     * @param year The release year for filtering.
+     * @param page The page number for pagination.
+     * @param size The number of movies to display per page.
+     * @return A ResponseEntity containing a Page of movies filtered by release year.
+     */
+    @Override
     public ResponseEntity<Page<Movie>> getByYear(Integer year, int page, int size) {
         try {
             Pageable pageable = PageRequest.of(page, size);
@@ -72,6 +98,14 @@ public class MovieServiceImpl implements MovieService {
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    /**
+     * Retrieve movies by genres in a paginated manner.
+     *
+     * @param genres The genres for filtering.
+     * @param page The page number for pagination.
+     * @param size The number of movies to display per page.
+     * @return A ResponseEntity containing a Page of movies filtered by genres.
+     */
     @Override
     public ResponseEntity<Page<Movie>> getByGenres(String genres, int page, int size) {
         try {
@@ -84,6 +118,14 @@ public class MovieServiceImpl implements MovieService {
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    /**
+     * Retrieve movies by title in a paginated manner.
+     *
+     * @param title The title for filtering.
+     * @param page The page number for pagination.
+     * @param size The number of movies to display per page.
+     * @return A ResponseEntity containing a Page of movies filtered by title.
+     */
     @Override
     public ResponseEntity<Page<Movie>> getByTitle(String title, int page, int size) {
         try {
@@ -95,5 +137,4 @@ public class MovieServiceImpl implements MovieService {
         }
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
 }
