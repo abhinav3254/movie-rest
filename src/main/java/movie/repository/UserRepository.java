58a108bb06717e4c670c7893505aa3e7a0996690
abhinav3254 <abhinav3254@gmail.com>
2023-10-26 18:35:52 +0530

    comments

--- src/main/java/movie/repository/UserRepository.java
+++ src/main/java/movie/repository/UserRepository.java
@@ -5,10 +5,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+/**
+ * The UserRepository interface defines data access methods for interacting with user data
+ * in the underlying database. It extends the JpaRepository interface for basic CRUD operations.
+ */
 @Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
-    @Query(nativeQuery = true,value = "select * from user where username=:username")
+    /**
+     * Retrieve a user by their username.
+     *
+     * @param username The username used for user retrieval.
+     * @return The user entity corresponding to the specified username.
+     */
+    @Query(nativeQuery = true, value = "select * from user where username = :username")
     User getUserByUserName(String username);
-
 }
