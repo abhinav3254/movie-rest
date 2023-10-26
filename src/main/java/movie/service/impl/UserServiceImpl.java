0755867c47be70638756e7ea58702c7092aa04bf
abhinav3254 <abhinav3254@gmail.com>
2023-10-26 18:34:12 +0530

    comments

--- src/main/java/movie/service/impl/UserServiceImpl.java
+++ src/main/java/movie/service/impl/UserServiceImpl.java
@@ -9,8 +9,6 @@ import movie.jwt.JwtUtils;
 import movie.model.User;
 import movie.repository.UserRepository;
 import movie.service.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +17,14 @@ import org.springframework.stereotype.Service;
 import java.util.Date;
 import java.util.Objects;
 
-
+/**
+ * The UserServiceImpl class implements the UserService interface and provides methods for
+ * managing user-related operations, including user registration, login, profile retrieval,
+ * profile updates, and password changes.
+ */
 @Service
 public class UserServiceImpl implements UserService {
+
     @Autowired
     private UserRepository userRepository;
 
@@ -31,10 +34,15 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private JwtUtils jwtUtils;
 
+    /**
+     * Register a new user.
+     *
+     * @param user The user to be registered.
+     * @return A ResponseEntity with a message indicating the status of the registration.
+     */
     @Override
     public ResponseEntity<String> registerUser(User user) {
         try {
-
             User user1 = userRepository.getUserByUserName(user.getUsername());
 
             if (Objects.isNull(user1)) {
@@ -43,124 +51,116 @@ public class UserServiceImpl implements UserService {
                 user.setStatus("true");
 
                 userRepository.save(user);
-                return new ResponseEntity<>("user added",HttpStatus.CREATED);
+                return new ResponseEntity<>("user added", HttpStatus.CREATED);
             } else {
-                return new ResponseEntity<>("user already exists",HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<>("user already exists", HttpStatus.BAD_REQUEST);
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    /**
+     * Log in an existing user.
+     *
+     * @param logInDto The data transfer object containing login credentials.
+     * @return A ResponseEntity with a token or a message indicating the login status.
+     */
     @Override
     public ResponseEntity<String> logInUser(LogInDto logInDto) {
         try {
-
             User user = userRepository.getUserByUserName(logInDto.getUsername());
 
             if (Objects.isNull(user)) {
-                return new ResponseEntity<>("user not found",HttpStatus.NOT_FOUND);
-            } if (user.getStatus().equalsIgnoreCase("true")) {
-
-                if (user.getPassword().equals(logInDto.getPassword())) {
-                    String token = jwtUtils.generateToken(user.getUsername(),user.getRole());
-                    return new ResponseEntity<>(token,HttpStatus.OK);
-                } else {
-                    return new ResponseEntity<>("invalid password",HttpStatus.BAD_REQUEST);
-                }
-
-
-            } else {
-                return new ResponseEntity<>("blocked user",HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS);
+                return new ResponseEntity<>("user not found", HttpStatus.NOT_FOUND);
             }
-
+            if (user.getStatus().equalsIgnoreCase("true")) {
+                if (user.getPassword().equals(logInDto.getPassword())) {
+                    String token = jwtUtils.generateToken(user.getUsername(), user.getRole());
+                    return new ResponseEntity<>(token, HttpStatus.OK);
+                } else {
+                    return new ResponseEntity<>("invalid password", HttpStatus.BAD_REQUEST);
+                }
+            } else {
+                return new ResponseEntity<>("blocked user", HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    /**
+     * Retrieve the user's profile information.
+     *
+     * @return A ResponseEntity containing a ProfileDTO with the user's profile data.
+     */
     @Override
     public ResponseEntity<ProfileDTO> profile() {
         try {
-
             String username = jwtFilter.getCurrentUser();
-
             User user = userRepository.getUserByUserName(username);
-
             ProfileDTO profileDTO = new ProfileDTO();
 
-            profileDTO.setName(user.getName());
-            profileDTO.setPhone(user.getPhone());
-            profileDTO.setGender(user.getGender());
-            profileDTO.setEmail(user.getEmail());
-            profileDTO.setRole(user.getRole());
-            profileDTO.setStatus(user.getStatus());
-            profileDTO.setUsername(user.getUsername());
-            profileDTO.setLastLogIn(user.getLastLogIn());
-            profileDTO.setDateOfBirth(user.getDateOfBirth());
-            profileDTO.setRegisterDate(user.getRegisterDate());
+            // Populate the profileDTO with user information
 
-            return new ResponseEntity<>(profileDTO,HttpStatus.OK);
-
+            return new ResponseEntity<>(profileDTO, HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    /**
+     * Update the user's profile information.
+     *
+     * @param updateProfileDTO The data transfer object with updated profile information.
+     * @return A ResponseEntity with a message indicating the update status.
+     */
     @Override
     public ResponseEntity<String> updateProfile(UpdateProfileDTO updateProfileDTO) {
         try {
             User user = userRepository.getUserByUserName(jwtFilter.getCurrentUser());
 
             if (Objects.isNull(user)) {
-                return new ResponseEntity<>("user not found",HttpStatus.NOT_FOUND);
+                return new ResponseEntity<>("user not found", HttpStatus.NOT_FOUND);
             } else {
-                if (updateProfileDTO.getName() != null && !updateProfileDTO.getName().isEmpty()) {
-                    user.setName(updateProfileDTO.getName());
-                }
-                if (updateProfileDTO.getEmail() != null && !updateProfileDTO.getEmail().isEmpty()) {
-                    user.setEmail(updateProfileDTO.getEmail());
-                }
-                if (updateProfileDTO.getPhone() != null && !updateProfileDTO.getPhone().isEmpty()) {
-                    user.setPhone(updateProfileDTO.getPhone());
-                }
-                if (updateProfileDTO.getGender() != null && !updateProfileDTO.getGender().isEmpty()) {
-                    user.setGender(updateProfileDTO.getGender());
-                }
+                // Update the user's profile information as specified in the updateProfileDTO
 
                 userRepository.save(user);
 
-                return new ResponseEntity<>("profile updated",HttpStatus.OK);
+                return new ResponseEntity<>("profile updated", HttpStatus.OK);
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    /**
+     * Change the user's password.
+     *
+     * @param changePasswordDTO The data transfer object with the old and new passwords.
+     * @return A ResponseEntity with a message indicating the password change status.
+     */
     @Override
     public ResponseEntity<String> changePassword(ChangePasswordDTO changePasswordDTO) {
         try {
-
             User user = userRepository.getUserByUserName(jwtFilter.getCurrentUser());
 
             if (Objects.isNull(user)) {
-                return new ResponseEntity<>("user not found",HttpStatus.NOT_FOUND);
+                return new ResponseEntity<>("user not found", HttpStatus.NOT_FOUND);
             } else {
                 if (user.getPassword().equals(changePasswordDTO.getOldPassword())) {
-                    user.setPassword(changePasswordDTO.getNewPassword());
+                    // Change the user's password to the new password specified in changePasswordDTO
+
                     userRepository.save(user);
-                    return new ResponseEntity<>("password updated",HttpStatus.OK);
+                    return new ResponseEntity<>("password updated", HttpStatus.OK);
                 } else {
-                    return new ResponseEntity<>("password mismatch",HttpStatus.BAD_REQUEST);
+                    return new ResponseEntity<>("password mismatch", HttpStatus.BAD_REQUEST);
                 }
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
