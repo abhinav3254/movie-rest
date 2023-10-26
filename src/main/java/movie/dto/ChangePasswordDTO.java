822914205a267bb58a159d695ec6a87d7d61f450
abhinav3254 <abhinav3254@gmail.com>
2023-10-26 18:37:22 +0530

    comments

--- src/main/java/movie/dto/ChangePasswordDTO.java
+++ src/main/java/movie/dto/ChangePasswordDTO.java
@@ -1,10 +1,19 @@
 package movie.dto;
 
-
 import lombok.Data;
 
+/**
+ * The ChangePasswordDTO class represents a data transfer object for changing a user's password.
+ */
 @Data
 public class ChangePasswordDTO {
-    String oldPassword;
-    String newPassword;
+    /**
+     * The old password that the user wants to change.
+     */
+    private String oldPassword;
+
+    /**
+     * The new password that the user wants to set.
+     */
+    private String newPassword;
 }
