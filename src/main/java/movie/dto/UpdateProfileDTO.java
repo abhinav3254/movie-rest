df686e5ffd612c6a2be538975dec6de35d6c0cb5
abhinav3254 <abhinav3254@gmail.com>
2023-10-26 18:39:03 +0530

    comments

--- src/main/java/movie/dto/UpdateProfileDTO.java
+++ src/main/java/movie/dto/UpdateProfileDTO.java
@@ -1,14 +1,30 @@
 package movie.dto;
 
-
 import lombok.Data;
 
+/**
+ * The UpdateProfileDTO class represents a data transfer object for updating user profile information.
+ */
 @Data
 public class UpdateProfileDTO {
 
+    /**
+     * The user's updated name.
+     */
     private String name;
-    private String email;
-    private String phone;
-    private String gender;
 
+    /**
+     * The user's updated email address.
+     */
+    private String email;
+
+    /**
+     * The user's updated phone number.
+     */
+    private String phone;
+
+    /**
+     * The user's updated gender.
+     */
+    private String gender;
 }
