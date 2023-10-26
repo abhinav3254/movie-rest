10da77041672a7630d4bbbfca56b447176d9e1fc
abhinav3254 <abhinav3254@gmail.com>
2023-10-26 18:37:54 +0530

    comments

--- src/main/java/movie/dto/LogInDto.java
+++ src/main/java/movie/dto/LogInDto.java
@@ -1,10 +1,21 @@
 package movie.dto;
 
+
 import lombok.Data;
 
+/**
+ * The LogInDto class represents a data transfer object for user login credentials.
+ */
 @Data
 public class LogInDto {
 
+    /**
+     * The username used for authentication.
+     */
     private String username;
+
+    /**
+     * The user's password used for authentication.
+     */
     private String password;
 }
